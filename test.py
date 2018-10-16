72f5170cd8f57277e0563fb62045363d4566e376
Edward Cree <ec429@cantab.net>
2018-10-16 09:24:03 +0100

    Homogenised the numbers for Delta and Kerolox
    Mainly to stop thrust going backwards on upgrades.

--- test.py
+++ test.py
@@ -47,10 +47,6 @@ class EngineClass(object):
         tothrust = toTL.maxThrust * tf
         return tothrust, self.calc(totl, tothrust, toTL.maxIgnitions - ni)
 
-### The numbers used below for the various TLs are designed to match the evolution of specific engine families.
-### This sometimes makes for slightly odd trajectories, particularly the up-and-down thrust of the AJ10s.
-### Before actually turning these into game configs, probably the numbers should be smoothed and homogenised.
-
 Aggregat = EngineClass('A', {'Ethanol75': 0.5125, 'LqdOxygen': 0.4875}) # let's not faff about with HTP
 Aggregat.setTL(0, 'unlockParts', 400, 203, 239, 0, 1.14, 200, 70)
 Aggregat.setTL(1, 'rocketryTesting', 360, 220, 255, 0, 1.12, 900, 115) # really should be Hydyne/LOx, but who cares
@@ -79,11 +75,11 @@ print
 
 Delta = EngineClass('D', {'UDMH': 0.4281, 'IRFNA-III': 0.5719}) # again, the propellants should vary per TL but who cares
 Delta.setTL(0, 'orbitalRocketry1956', 42, 240, 271, 1, 0.1, 200, 115)
-Delta.setTL(1, 'orbitalRocketry1958', 41, 238, 267, 1, 0.095, 176, 150)
-Delta.setTL(2, 'orbitalRocketry1959', 41.5, 240, 270, 1, 0.095, 196, 150)
-Delta.setTL(3, 'orbitalRocketry1960', 44, 240, 270, 1, 0.098, 204, 150)
-Delta.setTL(4, 'orbitalRocketry1961', 45, 215, 278, 4, 0.11, 336, 300)
-Delta.setTL(5, 'orbitalRocketry1964', 45.5, 100, 311, 4, 0.134, 405, None)
+Delta.setTL(1, 'orbitalRocketry1958', 44, 238, 267, 1, 0.101, 190, 150)
+Delta.setTL(2, 'orbitalRocketry1959', 46, 240, 270, 1, 0.1032, 220, 150)
+Delta.setTL(3, 'orbitalRocketry1960', 48, 240, 270, 1, 0.105, 225, 150)
+Delta.setTL(4, 'orbitalRocketry1961', 50, 215, 278, 4, 0.1194, 384, 300)
+Delta.setTL(5, 'orbitalRocketry1964', 52, 100, 311, 4, 0.149, 475, None)
 Delta.setTL(6, 'orbitalRocketry1972', 54, 215, 315, 16, 0.121, 472, None)
 Delta.setTL(7, 'orbitalRocketry1986', 56, 215, 319.2, 16, 0.122, 540, None)
 print Delta.calc(0, 33.8, 1) # AJ10-37: .084, 150
@@ -108,9 +104,9 @@ Kerolox.setTL(4, 'orbitalRocketry1961', 1200, 245, 289, 0, 0.77, 268, 150)
 Kerolox.setTL(5, 'orbitalRocketry1965', 1280, 265, 296, 0, 1.175, 275, 180)
 Kerolox.setTL(6, 'orbitalRocketry1972', 1275, 264, 295, 0, 1.225, 260, 240)
 Kerolox.setTL(7, 'orbitalRocketry1986', 1320, 255, 302, 0, 1.306, 328, 285)
-Kerolox.setTL(8, 'orbitalRocketry2004', 640, 267, 304.8, 1, 0.79, 260, 300)
-Kerolox.setTL(9, 'orbitalRocketry2009', 960, 282, 311, 4, 0.59, 248, 350)
-Kerolox.setTL(10,'orbitalRocketry2014', 1200, 288.5, 311, 4, 0.584, 244, 350)
+Kerolox.setTL(8, 'orbitalRocketry2004', 1400, 267, 304.8, 1, 1.48, 640, 300)
+Kerolox.setTL(9, 'orbitalRocketry2009', 1600, 282, 311, 4, 0.89, 460, 350)
+Kerolox.setTL(10,'orbitalRocketry2014', 1800, 288.5, 311, 4, 0.81, 400, 350)
 print Kerolox.calc(0, 600.5, 0) # S-3: .907, 300
 print Kerolox.calc(1, 758.7, 0) # S-3D: .907, 301
 print Kerolox.calc(2, 774, 0) # LR79-NA-9: .93421, 330
