7bbfd20718e3c456dc124d9dd301791c8f62b672
Edward Cree <ec429@cantab.net>
2020-06-20 01:25:24 +0100

    SPE 0.3

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -10,17 +10,17 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration ("")]
 [assembly: AssemblyCompany ("")]
 [assembly: AssemblyProduct ("KSPSPE")]
-[assembly: AssemblyCopyright ("Copyright © 2018")]
+[assembly: AssemblyCopyright ("Copyright © 2018-20")]
 [assembly: AssemblyTrademark ("")]
 [assembly: AssemblyCulture ("")]
 
 [assembly: Guid("349cd4dd-fbd9-495e-aea9-73b036736bd8")]
 
-[assembly: AssemblyVersion("0.2")]
-[assembly: AssemblyFileVersion("0.2.0")]
+[assembly: AssemblyVersion("0.3")]
+[assembly: AssemblyFileVersion("0.3.0")]
 
 // Use KSPAssembly to allow other DLLs to make this DLL a dependency in a
 // non-hacky way in KSP.  Format is (AssemblyProduct, major, minor), and it
 // does not appear to have a hard requirement to match the assembly version.
-[assembly: KSPAssembly("SPEngine", 0, 2)]
+[assembly: KSPAssembly("SPEngine", 0, 3)]
 [assembly: KSPAssemblyDependency("RealFuels", 12, 6)]
